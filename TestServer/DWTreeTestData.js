3bdaa100c962e3d4cace4a835f09024b129a80d4
menyin <845257580@qq.com>
2017-01-22 15:58:45 +0800

    dd

---
+++ TestServer/DWTreeTestData.js
@@ -0,0 +1,74 @@
+[
+    {
+        "id": "0c47962f-4d81-4efc-bafc-d82dd7304131",
+        "text": "刷新",
+        "value": "lr-replace",
+        "img": "/FrontEndFramework/images/Icon16/arrow_refresh.png",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    },
+    {
+        "id": "f77906f3-b8f7-45f8-9625-01d26e69771f",
+        "text": "新增",
+        "value": "lr-add",
+        "img": "/FrontEndFramework/images/Icon16/add.png",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    },
+    {
+        "id": "a6b591f5-fd89-42df-9ead-d188433f5736",
+        "text": "编辑",
+        "value": "lr-edit",
+        "img": "/FrontEndFramework/images/Icon16/edit.gif",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    },
+    {
+        "id": "5b3c839d-905e-4cee-aaf3-2cc22ec074a3",
+        "text": "删除",
+        "value": "lr-delete",
+        "img": "/FrontEndFramework/images/Icon16/DeleteRed.png",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    },
+    {
+        "id": "33f9c756-49a0-4d73-ad92-a36b93bc0c19",
+        "text": "详细",
+        "value": "lr-detail",
+        "img": "/FrontEndFramework/images/Icon16/application_view_detail.png",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    },
+    {
+        "id": "012a9ac9-f182-402e-8841-44680471177e",
+        "text": "离开",
+        "value": "lr-leave",
+        "img": "/FrontEndFramework/images/Icon16/back.png",
+        "parentnodes": "0",
+        "showcheck": false,
+        "isexpand": true,
+        "complete": true,
+        "hasChildren": false,
+        "ChildNodes": []
+    }
+]
